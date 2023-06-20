6063601294f84c1133251750e153ab3483804602
riefive <rie.five@gmail.com>
2023-06-20 18:34:58 +0700

    chore: modify app vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
-import HelloWorld from '@/components/HelloWorldDemo.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorldDemo from '@/components/HelloWorldDemo.vue';
 </script>
 
 <template>
-  <hello-world />
+  <hello-world-demo />
+  <div class="flex flex-col justify-center items-center gap-1">
+    <hello-world />
+  </div>
 </template>
